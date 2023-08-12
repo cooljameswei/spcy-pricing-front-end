fd6cdaec23c3f076bcd5a1db54330255ea6a94a1
binary3oul <solarteacostaw@gmail.com>
2023-08-12 05:44:00 -0500

    added yes/no functionality

--- pricing.js
+++ pricing.js
@@ -1,3 +1,85 @@
+$('.add-event').click(function() {
+    $('.items').append(`
+        <div class="flex-column item" style="display:flex">
+            <div class="d-flex flex-column flex-lg-row mt-4 mb-4 justify-content-md-between">
+                <div class="timepicker-wrapper d-flex flex-1 flex-column flex-lg-row align-items-lg-center">
+                    <label class="w-md-100">Date</label> <input type="date" class="datetime text-center Date" required/>
+                </div>
+                <div class="d-flex flex-column flex-lg-row justify-content-between gap-lg-5">
+                    <div class="timepicker-wrapper d-flex justify-content-lg-center flex-column flex-lg-row">
+                        <label class="ml-lg-1">From</label> <input class="datetime text-center StartTime" type="time" name="from" required>
+                    </div>
+                    <div class="timepicker-wrapper d-flex justify-content-lg-center flex-column flex-lg-row">
+                        <label class="">To</label> <input class="ml-1 datetime text-center EndTime" type="time" name="to" required>
+                    </div>
+                </div>
+            </div>
+            <div class="locations-header d-flex align-items-center justify-content-between">
+                <h4>Locations</h4>  
+                <div class="text-primary text-center add add-location"><i class="fa fa-plus"></i> Add Location</div>
+            </div>
+
+            <ul class="list-group list-group-flush locations">
+                <li class="list-group-item location pt-4 pb-4 d-flex flex-column gap-4">
+                    <div class="location-address">
+                        <label for="address" class="form-label text-white">Location Address</label>
+                        <input type="text" class="form-control datetime Address" required>
+                    </div>
+                    <div class="d-flex justify-content-between align-items-center flex-column flex-sm-row gap-3">
+                        <div class="form-check d-flex align-items-center gap-3">
+                            <input type="checkbox" class="form-check-input text-white IsGta" style="display: inline-block;" checked>
+                            <label class="form-check-label ml-4 text-white">Within GTA?</label>
+                        </div>
+
+                        <div class="text-danger text-center add remove-location" onclick="$(this).closest('.location').remove()"><i class="fa fa-minus"></i> Remove This Location</div>
+                    </div>
+                </li>
+            </ul>
+            <button class="btn btn-danger remove-event" type="button" onclick="$(this).closest('.item').fadeOut()"><i class="fa fa-minus"></i> Remove This Event</button>
+
+        </div>
+    `)
+})
+
+$('.remove-event').click(function() {
+    $(this).closest('.item').fadeOut()
+})
+
+$(document).on('click', '.add-location', function() {
+    $(this).parent().siblings('.locations').append(
+        `<li class="list-group-item location pt-4 pb-4 d-flex flex-column gap-4">
+            <div class="location-address">
+                <label for="address" class="form-label text-white">Location Address</label>
+                <input type="text" class="form-control datetime Address" required>
+            </div>
+            <div class="d-flex justify-content-between align-items-center flex-column flex-sm-row gap-3">
+                <div class="form-check d-flex align-items-center gap-3">
+                    <input type="checkbox" class="form-check-input text-white IsGta" style="display: inline-block;" checked>
+                    <label class="form-check-label ml-4 text-white">Within GTA?</label>
+                </div>
+
+                <div class="text-danger text-center add remove-location" onclick="$(this).closest('.location').remove()"><i class="fa fa-minus"></i> Remove This Location</div>
+            </div>
+        </li>`
+    )
+});
+
+$('.show-more').click(function() {
+    let active = !$(this).data('active')
+    $(this).data('active', active)
+    let salesDescription = $(this).closest('.list-group-item').find('.sales-description')
+    if(active){
+        $(this).find('img').attr('src', './images/info-active.png')
+        salesDescription.css({"maxHeight": "400px"})
+    }
+    else{
+        $(this).find('img').attr('src', './images/info.png')
+        salesDescription.css({"maxHeight": 0})
+    }
+})
+
+var RequestId = 0;
+
 $('.btn-request-quote').click(function() {
     $(this).fadeOut()
     $('#generate-quote-form').fadeIn()
@@ -47,6 +129,7 @@ $('#generate-quote-form').submit(function(e) {
 
 		success: function (data) {
             const { CalculatedPrice, Message, BreakDown, WillAddAdditionalCost } = data
+            RequestId = data.RequestId
             $('#result').html(`
             <div class="alert alert-success" role="alert">
                 ${Message}
@@ -70,19 +153,21 @@ $('#generate-quote-form').submit(function(e) {
                 <div class="text-center d-flex justify-content-around flex-sm-row flex-column gap-4">
                     <span>Are you okay with this price and booking me?</span>
                     <div class="text-center">
-                        <button class="btn btn-success">Yes</button>
+                        <button class="btn btn-success btn-yes">Yes</button>
                         <button class="btn btn-light btn-no">No</button>
                     </div>
                 </div>
                 <div class="negotiate-budget">
                     <hr>
-                    <div class="form-group d-flex justify-content-between flex-sm-row flex-column">
-                        <label for="budget">Please let me know your budget</label>
-                        <input type="number" id="budget" class="form-control mt-2 mb-2 m-sm-0" style="max-width: 240px;"/>
-                    </div>
-                    
-                    <label class="mt-0 mt-sm-2">I can customize my service to meet your budget so please feel free to let me know best price you are willing to pay.</label>
-                    <div class="text-center mt-2"><button class="btn btn-primary">Submit</button></div>
+                    <form method="POST" id="form-negotiate">
+                        <div class="form-group d-flex justify-content-between flex-sm-row flex-column">
+                            <label for="budget">Please let me know your budget</label>
+                            <input type="number" id="budget" class="form-control mt-2 mb-2 m-sm-0" style="max-width: 240px;" required/>
+                        </div>
+                        
+                        <label class="mt-0 mt-sm-2">I can customize my service to meet your budget so please feel free to let me know best price you are willing to pay.</label>
+                        <div class="text-center mt-2"><button type="submit" class="btn btn-primary btn-submit">Submit</button></div>
+                    </form>
                 </div>
             </div>
             `)
@@ -95,11 +180,61 @@ $('#generate-quote-form').submit(function(e) {
 	});
 })
 
+$(document).on('click', '.btn-yes', function() {
+    const data = {
+        RequestId,
+        Agreed: true
+    }
+    
+    $.ajax({
+        url: "https://spicyvision.com/QuoteService/UpdateCustomerResponse",
+		method: "POST",
+		dataType: "json",
+        data: JSON.stringify(data),
+        contentType: "application/json",
+        success: function(response) {
+            $('#result').html(
+                `<div class="alert alert-primary" role="alert">
+                    ${response.Message}
+                </div>`
+            )
+        }
+    })
+})
+
 $(document).on('click', '.btn-no', function() {
     let negotiateBudget = $('.negotiate-budget')
-    console.log(negotiateBudget)
     negotiateBudget.css({maxHeight: 240})
+    
     $(this).prop('disabled', true)
+    
+});
+
+$(document).on('submit', '#form-negotiate', function(e) {
+    e.preventDefault()
+    let Budget = $('#budget').val()
+    Budget = Number(Budget)
+    const data = {
+        RequestId,
+        Agreed: false,
+        WantsToNegotiate: true,
+        Budget
+    }
+    $.ajax({
+        url: "https://spicyvision.com/QuoteService/UpdateCustomerResponse",
+		method: "POST",
+		dataType: "json",
+        data: JSON.stringify(data),
+        contentType: "application/json",
+        success: function(response) {
+            $('#result').html(
+                `<div class="alert alert-primary" role="alert">
+                    ${response.Message}
+                </div>`
+            )
+        }
+    })
+    
 });
 
 $('.card-1 .form-check-input').click(function() {
